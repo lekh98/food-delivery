82a32766d339fafe25395dba086e6db92be10354
lekhnath <142299968+lekh98@users.noreply.github.com>
2024-06-23 10:40:04 +0545

    Update StoreContext.jsx

--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -8,7 +8,7 @@ const StoreContextProvider = (props) =>{
     const [cartItems, setCartItems] = useState({})
     
 
-    const url = "http://localhost:4000";
+    const url = "https://food-delivery-backend-qs22.onrender.com";
 
     const [token,setToken] = useState("")
 
