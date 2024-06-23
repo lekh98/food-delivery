f5e72f2407489277744f76251bc477440c6f7cca
lekhnath <142299968+lekh98@users.noreply.github.com>
2024-06-23 11:00:59 +0545

    Update Navbar.jsx

--- frontend/src/components/Navbar/Navbar.jsx
+++ frontend/src/components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ const Navbar = ({setShowLogin}) => {
             <a href='#explore-menu' onClick={()=>setMenu("menu")} className={menu === "menu"?"active":""}>menu</a>
             <a href='#app-download' onClick={()=>setMenu("mobile-app")} className={menu === "mobile-app"?"active":""}>mobile-app</a>
             <a href='#footer' onClick={()=>setMenu("contact-us")} className={menu === "contact-us"?"active":""}>contact-us</a>
-            <Link>Admin</Link>
+            <Link to="https://food-delivery-aio2.onrender.com">Admin</Link>
         </ul>
         <div className="navbar-right">
             <img src={assets.search_icon} alt="" />
